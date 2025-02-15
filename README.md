aee90542c4a99f0875f20800a8da7471ac2a5d84
rafaelcesar0@proton.me <rafaelcesar0>
2025-02-15 02:28:34 -0300

    refactor: update

--- README.md
+++ README.md
@@ -34,67 +34,6 @@ sudo apt install -y curl git unzip fish
 
 Command: `fish`
 
-```sh
-chsh -s (which fish)
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-```
-
-```sh
-# Instala Bass
-omf install bass
-```
-
-```sh
-# Instala Starship
-curl -sS https://starship.rs/install.sh | sh -s -- -y
-mkdir -p ~/.config
-cp -f "$PWD/config/starship.toml" ~/.config/
-```
-
-```sh
-# Instala Zoxide
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-```
-
-```sh
-# Instala FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-```
-
-```sh
-# Instala NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-```
-
-```sh
-# Configurar Fish
-mkdir -p ~/.config/fish
-cp -f "$PWD/config/config.fish" ~/.config/fish/
-```
-
-Command: `exec fish`
-
-```sh
-# Instala Node LTS
-nvm install --lts
-nvm use --lts
-nvm alias default 'lts/*'
-npm update -g npm
-```
-
-```sh
-# Instala PNPM
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-```
-
-```sh
-# Instala Bun
-curl -fsSL https://bun.sh/install | bash
-```
-
-Command: `exec fish`
-
 ## Download repository
 
 ### HTTPS
@@ -113,7 +52,7 @@ git clone git@github.com:rafaelcesar0/my-fish-setup.git
 ## 3. Run the [*install.sh*](https://github.com/rafaelcesar0/my-fish-setup/blob/main/install.sh) script
 
 ```bash
-cd "$(pwd)/my-fish-setup" # Access the repository folder
+cd ./my-fish-setup # Access the repository folder
 chmod +x ./install.sh # Permission to run the script
 ./install.sh # Run script
 ```
--- install.sh
+++ install.sh
@@ -1,43 +1,133 @@
-#!/bin/bash
+#!/usr/bin/env fish
 
-# Atualiza pacotes e instala dependências
-sudo apt update && sudo apt install -y curl git unzip
+function log_info
+    set_color green
+    echo "➜ $argv"
+    set_color normal
+end
 
-# Instala Bass
-omf install bass
+function log_error
+    set_color red
+    echo "✖ $argv"
+    set_color normal
+    return 1
+end
+
+function check_command
+    if not type -q $argv[1]
+        log_error "Comando '$argv[1]' não encontrado. Instalação falhou."
+        exit 1
+    end
+end
+
+# Verifica se já está rodando o Fish
+if not status is-interactive
+    log_error "Este script deve ser executado interativamente no Fish shell"
+    exit 1
+end
+
+# Muda o shell padrão para Fish
+log_info "Configurando Fish como shell padrão..."
+if not grep -q (which fish) /etc/shells
+    echo (which fish) | sudo tee -a /etc/shells
+end
+chsh -s (which fish)
+
+# Instala Oh My Fish com verificação
+log_info "Instalando Oh My Fish..."
+if not test -d ~/.local/share/omf
+    curl -L https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+else
+    log_info "Oh My Fish já está instalado"
+end
 
 # Instala Starship
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+log_info "Instalando Starship..."
+if not type -q starship
+    curl -sS https://starship.rs/install.sh | sh -s -- -y
+    or log_error "Falha ao instalar Starship"
+end
+
+# Configura Starship
 mkdir -p ~/.config
 cp -f "$PWD/config/starship.toml" ~/.config/
+or log_error "Falha ao copiar configuração do Starship"
 
 # Instala Zoxide
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+log_info "Instalando Zoxide..."
+if not type -q zoxide
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    or log_error "Falha ao instalar Zoxide"
+end
 
 # Instala FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+log_info "Instalando FZF..."
+if not test -d ~/.fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all
+    or log_error "Falha ao instalar FZF"
+end
+
+# Instala plugins do Fish
+log_info "Instalando plugins do Fish..."
+omf install https://github.com/fabioantunes/fish-nvm
+omf install bass
+or log_error "Falha ao instalar plugins do Fish"
 
 # Instala NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+log_info "Instalando NVM..."
+if not test -d ~/.nvm
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    or log_error "Falha ao instalar NVM"
+end
 
-# Configurar Fish
+# Configura Fish
+log_info "Configurando Fish..."
 mkdir -p ~/.config/fish
 cp -f "$PWD/config/config.fish" ~/.config/fish/
+or log_error "Falha ao copiar configuração do Fish"
 
-# Aplica mudanças
-exec fish
+# Configura NVM e instala Node LTS
+log_info "Configurando NVM e instalando Node LTS..."
+function nvm
+    bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+# Verifica se o NVM está funcionando
+if not functions -q nvm
+    log_error "NVM não foi configurado corretamente"
+    exit 1
+end
 
 nvm install --lts
+or log_error "Falha ao instalar Node LTS"
+
 nvm use --lts
 nvm alias default 'lts/*'
 npm update -g npm
+or log_error "Falha ao configurar Node"
 
 # Instala PNPM
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+log_info "Instalando PNPM..."
+if not type -q pnpm
+    curl -fsSL https://get.pnpm.io/install.sh | sh -
+    or log_error "Falha ao instalar PNPM"
+end
 
 # Instala Bun
-curl -fsSL https://bun.sh/install | bash
+log_info "Instalando Bun..."
+if not type -q bun
+    curl -fsSL https://bun.sh/install | bash
+    or log_error "Falha ao instalar Bun"
+end
 
-# Aplica mudanças
-exec fish
+# Verifica se tudo foi instalado corretamente
+log_info "Verificando instalações..."
+set required_commands fish omf starship zoxide fzf node npm pnpm bun
+
+for cmd in $required_commands
+    check_command $cmd
+end
+
+log_info "✓ Configuração concluída com sucesso!"
+log_info "Por favor, reinicie o terminal para aplicar todas as mudanças."
